efd9369751344e2a402d29c1ba83f7a2d734e6de
Ashley Hindle <hey@ashleyhindle.com>
2016-10-15 18:49:30 +0100

    Initial setup of the oauth2 wunderlist API

---
+++ src/Wunderlist.php
@@ -0,0 +1,56 @@
+<?php
+namespace TodoMove\OAuth2\Client\Provider;
+
+use League\OAuth2\Client\Provider\AbstractProvider;
+use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
+use League\OAuth2\Client\Provider\ResourceOwnerInterface;
+use League\OAuth2\Client\Token\AccessToken;
+use Psr\Http\Message\ResponseInterface;
+
+class Wunderlist extends AbstractProvider
+{
+    public function getBaseAuthorizationUrl()
+    {
+        return 'https://www.wunderlist.com/oauth/authorize';
+    }
+
+
+    public function getBaseAccessTokenUrl(array $params)
+    {
+        return 'https://www.wunderlist.com/oauth/access_token';
+    }
+
+    public function getResourceOwnerDetailsUrl(AccessToken $token)
+    {
+        return '';
+    }
+
+    protected function getDefaultScopes()
+    {
+        return [];
+    }
+
+    protected function getAuthorizationHeaders($token = null) {
+        return [
+            'X-Access-Token' => $token,
+            'X-Client-ID' => $this->clientId,
+        ];
+    }
+
+    protected function checkResponse(ResponseInterface $response, $data)
+    {
+        if ($response->getStatusCode() >= 400) {
+            throw new IdentityProviderException(
+                $data['error'],
+                isset($data['code']) ? (int) $data['code'] : $response->getStatusCode(),
+                $response
+            );
+        }
+    }
+
+    protected function createResourceOwner(array $response, AccessToken $token)
+    {
+        return null;
+    }
+
+}
